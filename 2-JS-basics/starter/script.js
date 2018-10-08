ffb9c0512f7e1905bc64a19fb2a1b28c5f3278ca
ryyna <ryyna@192.168.0.20>
2018-10-08 16:38:46 +0300

    Update

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,33 +1,34 @@
-//var firstName = 'John';
-//var age = 28;
-//
-//console.log('Name: ' + firstName + ', age: ' + age);
-//
-//var job, isMarried;
-//job = 'Teacher';
-//isMarried = false;
-//
-//console.log('Name: ' + firstName + 
-//			', age: ' + age +
-//			', job: ' + job +
-//			', married: ' + isMarried);
-//
-//
-//age = 'twenty eight';
-//job = 'driver';
-//	
-//	alert('Name: ' + firstName + 
-//			', age: ' + age +
-//			', job: ' + job +
-//			', married: ' + isMarried);
-//	
-//var lastName = prompt('What is his last name?');
-//console.log('Entered ' + lastName + ' for last name');
-//
-//var firstName = prompt('What is his first name?')
-//console.log('Entered ' + firstName + ' for first name.')
-//
-//console.log('Full name is ' + firstName + ' ' + lastName);
+/*
+var firstName = 'John';
+var age = 28;
+
+console.log('Name: ' + firstName + ', age: ' + age);
+
+var job, isMarried;
+job = 'Teacher';
+isMarried = false;
+
+console.log('Name: ' + firstName + 
+			', age: ' + age +
+			', job: ' + job +
+			', married: ' + isMarried);
+
+
+age = 'twenty eight';
+job = 'driver';
+	
+	alert('Name: ' + firstName + 
+			', age: ' + age +
+			', job: ' + job +
+			', married: ' + isMarried);
+	
+var lastName = prompt('What is his last name?');
+console.log('Entered ' + lastName + ' for last name');
+
+var firstName = prompt('What is his first name?')
+console.log('Entered ' + firstName + ' for first name.')
+
+console.log('Full name is ' + firstName + ' ' + lastName);
 
 var year, yearJohn, yearMark;
 year = 2018
@@ -43,3 +44,16 @@ console.log(year + 2);
 var johnOlder = ageJohn > ageMark;
 console.log(johnOlder);
 console.log(typeof johnOlder);
+*/
+
+var now = 2018;
+var yearJohn = 1981;
+var fullAge = 18;
+
+var isFullAge = now - yearJohn >= fullAge;
+console.log(isFullAge);
+
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = ageJohn + ageMark / 2;
+console.log(average);
