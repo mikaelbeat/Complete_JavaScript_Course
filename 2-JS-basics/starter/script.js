8900af1abcaf99907734858072a5974b1f19bae3
ryyna <ryyna@192.168.0.20>
2018-10-08 14:36:55 +0300

    Update

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,19 +1,45 @@
-// console.log('External script!');
+//var firstName = 'John';
+//var age = 28;
+//
+//console.log('Name: ' + firstName + ', age: ' + age);
+//
+//var job, isMarried;
+//job = 'Teacher';
+//isMarried = false;
+//
+//console.log('Name: ' + firstName + 
+//			', age: ' + age +
+//			', job: ' + job +
+//			', married: ' + isMarried);
+//
+//
+//age = 'twenty eight';
+//job = 'driver';
+//	
+//	alert('Name: ' + firstName + 
+//			', age: ' + age +
+//			', job: ' + job +
+//			', married: ' + isMarried);
+//	
+//var lastName = prompt('What is his last name?');
+//console.log('Entered ' + lastName + ' for last name');
+//
+//var firstName = prompt('What is his first name?')
+//console.log('Entered ' + firstName + ' for first name.')
+//
+//console.log('Full name is ' + firstName + ' ' + lastName);
 
-var firstName = 'John';
-console.log(firstName);
+var year, yearJohn, yearMark;
+year = 2018
+ageJohn = 28;
+ageMark = 33;
 
-var lastName = 'Smith';
-var age = 28;
+yearJohn = year - ageJohn;
+yearMark = year - ageMark;
 
-var fullAge = true;
+console.log(yearJohn);
+console.log(year + 2);
 
-console.log(fullAge);
-
-var job;
-
-console.log(job);
-
-job = 'Teacher';
-
-console.log(job);
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+console.log(typeof johnOlder);
