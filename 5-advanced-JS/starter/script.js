fe4dd943eca3d9d69392bcf783750ff177fb98e5
mikaelbeat <ryynanenphm@gmail.com>
2018-11-03 22:16:19 +0200

    Update

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,4 +1,103 @@
 
+// Bind, call and apply
+
+var john = {
+    name: 'John',
+    age: 26,
+    job: 'Teacher',
+    presentation: function(style, timeOfDay){
+        if (style === 'Formal'){
+            console.log('Good ' + timeOfDay + ' Ladies and gentlemen. I\'m ' + this.name + ', I\'m a ' +
+                                                this.job + ' and I\'m ' + this.age + ' years old.');
+        } else if (style == 'Friendly'){
+            console.log('Hey, what\'s up?. I\'m ' + this.name + ', I\'m a ' +
+            this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+        }
+    }
+}
+
+var emily = {
+    name: 'Emily',
+    age: 35,
+    job: 'Designer'
+};
+
+
+john.presentation('Formal','Morning')
+john.presentation.call(emily, 'Friendly', 'Afternoon');
+
+var johnFriedly = john.presentation.bind(john, 'Friendly');
+johnFriedly('Morning');
+johnFriedly('Night');
+
+var emilyFormal = john.presentation.bind(emily, 'Formal');
+emilyFormal('Afternoon');
+
+/*
+
+// Closures
+
+function retirement(retirementAge){
+    var a = ' years left until retirement.';
+        return function(yearOfBirth){
+            var age = 2016 -yearOfBirth;
+            console.log((retirementAge - age) + a);
+    }
+}
+
+var retirementUS = retirement(66);
+retirementUS(1990);
+
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementUS(1990);
+retirementGermany(1990);
+retirementIceland(1990);
+
+//retirement(66)(1990);
+
+console.log('*********************************');
+
+function interviewQuestion(job){
+    return function(name){
+        if (job == 'Designer'){
+            console.log(name + ', can you please explaon what UX design is?');
+        } else if (job === 'Teacher'){
+            console.log('What subject do you teach, ' + name + '?');
+        } else {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+interviewQuestion('Teacher')('John');
+
+
+
+// IIFE
+
+function game(){
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+}
+
+game();
+
+
+(function (){
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+})();
+
+
+(function (goodLuck){
+    var score = Math.random() * 10;
+    console.log(score >= 5 - goodLuck);
+})(5);
+
+
+
 // Functions returning functions
 
 function interviewQuestion(job){
@@ -31,7 +130,7 @@ interviewQuestion('Designer')('Julia');
 interviewQuestion('')('Logan');
 
 
-/*
+// Inner finctions
 
 var years = [1990, 1965, 1937, 2005, 1998];
 
