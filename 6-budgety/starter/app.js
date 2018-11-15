aea6503e218288937c18719e667143cbdf1090a2
Ryynänen <petri.ryynanen@accenture.com>
2018-11-15 18:29:36 +0200

    Update

---
+++ 6-budgety/starter/app.js
@@ -0,0 +1,31 @@
+var budgetController = (function(){
+
+var x = 23;
+
+var add = function(a){
+    return x + a;
+}
+
+return{
+    publicTest: function(b){
+        console.log(add(b));
+    }
+}
+
+})();
+
+
+
+var UIController = (function(){
+
+
+})();
+
+
+
+var controller = (function(budgetCtrl, UICtrl){
+
+
+// 12,36
+
+})();
