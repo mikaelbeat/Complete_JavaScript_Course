be79311005065e9375dc23b9123b6ee11fcd5664
mikaelbeat <ryynanenphm@gmail.com>
2018-11-18 22:22:40 +0200

    Update

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -8,6 +8,26 @@ var budgetController = (function(){
 
 var UIController = (function(){
 
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    };
+
+    return {
+        getInput: function(){
+            return {
+                // Will be either inc or exp
+                    type: document.querySelector(DOMstrings.inputType).value,
+                    description: document.querySelector(DOMstrings.inputDescription).value,
+                    value: document.querySelector(DOMstrings.inputValue).value
+                };
+        },
+            getDOMstring: function(){
+                return DOMstrings;
+            }
+    }
 
 })();
 
@@ -15,14 +35,19 @@ var UIController = (function(){
 
 var controller = (function(budgetCtrl, UICtrl){
 
+    var DOM = UIController.getDOMstring();
+
     var ctrlAddItem = function() {
+
+        var input = UICtrl.getInput();
+        console.log(input);
 
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event){
-        if (event.keyCode === 13){
+        if (event.keyCode === 13 || event.which === 13){
             ctrlAddItem();
         }
     });
