5dec06b7039b35c9125b0711fbdb9d129d7efcf9
mikaelbeat <ryynanenphm@gmail.com>
2018-11-20 15:07:17 +0200

    Update

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -20,6 +20,31 @@ var budgetController = (function(){
             exp: 0,
             inc: 0
         }
+    };
+    return {
+        addItem: function(type, des, val){
+            var newItem, ID;
+
+            // Create new ID
+            if (data.allItems[type].length > 0){
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+            // Create new item based on 'inc or 'exp' type
+            if (type === 'exp'){
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc'){
+                newItem = new Income(ID, des, val);
+            }
+            // Push it into data structure
+            data.allItems[type].push(newItem);
+            // Return the new element
+            return newItem;
+        },
+        testing: function(){
+            console.log(data);
+        }
     }
 })();
 
@@ -45,7 +70,6 @@ var UIController = (function(){
     }
 })();
 
-
 var controller = (function(budgetCtrl, UICtrl){
     var setupEventListers = function(){
         var DOM = UIController.getDOMstring();
@@ -58,7 +82,10 @@ var controller = (function(budgetCtrl, UICtrl){
     }
 
     var ctrlAddItem = function() {
-        var input = UICtrl.getInput();
+        var input, newItem;
+        input = UICtrl.getInput();
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
         console.log(input);
     };
 
