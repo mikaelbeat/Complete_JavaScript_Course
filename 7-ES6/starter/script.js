a70785dbc2b1d552d2ed8452f006d9c58a735157
Ryynänen Petri <ext.petri.ryynanen@ilmarinen.fi>
2018-12-13 15:57:46 +0200

    Update

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -13,7 +13,7 @@ let age6 = 23;
 name6 = 'Jane Smith';
 console.log(name6);
 
-*/
+
 
 // ES5
 function driversLicence(passedTest) {
@@ -38,3 +38,67 @@ function driversLicence6(passedTest) {
         console.log(firstName + ', born in ' + yearOfBirth + ' is now officially allowed to drive car.');
     }
 }
+
+
+
+// Lecture: Blocks and IIFEs
+
+// const and let defined variables are visible only in the block where they are defined.
+{
+    const a = 'First';
+    let b = 'Second';
+}
+
+console.log(a + ' ' + b);
+
+
+
+// Lecture: Strings
+
+let firstName = 'John';
+let lastName = 'Smith';
+const yearOfBirth = 1990;
+
+function calcAge(year){
+    return 2016 - year;
+}
+
+// ES5
+console.log('Name is ' + firstName + ', and last name is ' + lastName + ' is ' + calcAge(yearOfBirth) + ' years old.');
+
+// ES6
+console.log(`Name is ${firstName}, and last name is ${lastName} is ${calcAge(yearOfBirth)} years old.`);
+
+const n = `${firstName} ${lastName}`;
+console.log(n.startsWith('J'));
+console.log(n.endsWith('th'));
+console.log(n.includes('oh'));
+console.log(`${firstName} `.repeat(5));
+
+*/
+
+// Lecture: Arrow functions
+
+const years = [1990, 1965, 1982, 1937];
+
+
+// ES5
+var ages5 = years.map(function(element){
+    return 2018 - element;
+});
+console.log(ages5);
+
+// ES6
+let ages6 = years.map(element => 2018 - element);
+console.log(ages6);
+
+ages6 = years.map((element, index) => `Age element ${index + 1}: ${2018 - element}.`);
+console.log(ages6);
+
+ages6 = years.map((element, index) => {
+    const now = new Date().getFullYear();
+    const age = now - element;
+    return `Age element ${index + 1}: ${age}.`
+})
+
+console.log(ages6);
